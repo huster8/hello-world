489de35e667c0ded4257351ca04e5c22f37e7e8a
zachary huster <huster.8@gmail.com>
2024-02-14 13:25:52 -0500

    zh: 2.14.24 happy v-day / added 404 not found

--- git-blog/src/App.js
+++ git-blog/src/App.js
@@ -3,6 +3,7 @@ import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './Create';
 import ProjectDetails from './ProjectDetails';
+import NotFound from './NotFound';
 
 function App() {
   return (
@@ -19,6 +20,9 @@ function App() {
             </Route>
             <Route path="/projects/:id">
               <ProjectDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
             </Route>
           </Switch>
         </div>
